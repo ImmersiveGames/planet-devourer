57e7546d50932cb718682d832f6f3ad428fff34c
Renato Innocenti <renato.innocenti@gmail.com>
2026-07-04 13:22:09 -0300

    test(firstgame): add reset usage example

---
+++ Assets/_Project/Scripts/FirstGamePlayerResetProbe.cs
@@ -0,0 +1,42 @@
+using Immersive.Framework.Reset;
+using Immersive.Framework.Reset.Unity;
+using UnityEngine;
+namespace _Project.Scripts
+{
+    public sealed class FirstGamePlayerResetProbe : MonoBehaviour, IUnityResettable, IUnityResettableMetadata
+    {
+        private int _resetCount;
+        private Vector3 _baselinePosition;
+
+        public string ResetParticipantId => "firstgame.player.reset-probe";
+
+        public ResetParticipantRequiredness ResetRequiredness => ResetParticipantRequiredness.Required;
+
+        public int ResetOrder => 100;
+
+        public string ResetDisplayName => "FirstGame Player Reset Probe";
+
+        public string ResetSource => nameof(FirstGamePlayerResetProbe);
+
+        public string ResetReason => "firstgame.player.reset-probe";
+
+        private void Awake()
+        {
+            _baselinePosition = transform.position;
+        }
+
+        public ResetParticipantResult Reset(ResetContext context)
+        {
+            _resetCount++;
+            transform.position = _baselinePosition;
+
+            Debug.Log($"[FIRSTGAME_RESETTABLE] id='{ResetParticipantId}' resetCount='{_resetCount}' reason='{context.Reason}'");
+
+            return ResetParticipantResult.CreateSucceeded(
+                context.Participant,
+                nameof(FirstGamePlayerResetProbe),
+                context.Reason,
+                $"FirstGame player reset probe executed. resetCount='{_resetCount}'.");
+        }
+    }
+}
